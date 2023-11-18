2fb72c9f8b3240c410c4d7fed9b474d545fcb9ae
xtrdnrmnd <polinkakub@mail.ru>
2023-11-18 12:43:44 +0100

    minor fixes

--- src/NumbersInWords.java
+++ src/NumbersInWords.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 
 public class NumbersInWords {
     public static void main(String[] args) {
-        System.out.println("Insert the number:");
-        Scanner input = new Scanner(System.in);
-        int inputNumber = input.nextInt();
-        Logic logic = new Logic();
-        System.out.println(logic.mainFunction(inputNumber));
+        try {
+            System.out.println("Insert the number (from 1 to 99 999 999):");
+            Scanner input = new Scanner(System.in);
+            int inputNumber = input.nextInt();
+            Logic logic = new Logic();
+            System.out.println(logic.mainFunction(inputNumber));
+        } catch (Exception e) {
+            System.out.println("You have given the wrong input");
+        }
     }
 }
